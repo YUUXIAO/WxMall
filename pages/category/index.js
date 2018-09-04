0e66f93f63f00f65c249708447b5034a93bffcdd
yuxiao <1057305789@qq.com>
2018-09-04 10:22:40 +0800

    分类列表

---
+++ pages/category/index.js
@@ -0,0 +1,264 @@
+// pages/category/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    categoryNavigation: [
+      {
+        "name":"水果"
+      },
+      {
+        "name": "蔬菜"
+      },
+      {
+        "name": "服饰"
+      },
+      {
+        "name": "玩具"
+      }
+    ], products:[
+      {
+        "banner": "/images/bg1.jpg",
+        "title":'水果',
+        "product":[
+          {
+            "image":"/images/bg1.jpg",
+            "title":'水果1'
+          },
+          {
+            "image": "/images/bg1.jpg",
+            "title": '水果2'
+          },
+          {
+            "image": "/images/bg1.jpg",
+            "title": '水果3'
+          },
+          {
+            "image": "/images/bg1.jpg",
+            "title": '水果4'
+          },
+          {
+            "image": "/images/bg1.jpg",
+            "title": '水果5'
+          },
+          {
+            "image": "/images/bg1.jpg",
+            "title": '水果6'
+          },
+          {
+            "image": "/images/bg1.jpg",
+            "title": '水果7'
+          },
+          {
+            "image": "/images/bg1.jpg",
+            "title": '水果8'
+          },
+          {
+            "image": "/images/bg1.jpg",
+            "title": '水果9'
+          },
+          {
+            "image": "/images/bg1.jpg",
+            "title": '水果10'
+          }
+        ]
+      },
+      {
+        "banner": "/images/bg1.jpg",
+        "title": '蔬菜',
+        "product": [
+          {
+            "image": "/images/bg1.jpg",
+            "title": '蔬菜1'
+          },
+          {
+            "image": "/images/bg1.jpg",
+            "title": '蔬菜2'
+          },
+          {
+            "image": "/images/bg1.jpg",
+            "title": '蔬菜3'
+          },
+          {
+            "image": "/images/bg1.jpg",
+            "title": '蔬菜4'
+          },
+          {
+            "image": "/images/bg1.jpg",
+            "title": '蔬菜5'
+          },
+          {
+            "image": "/images/bg1.jpg",
+            "title": '蔬菜6'
+          },
+          {
+            "image": "/images/bg1.jpg",
+            "title": '蔬菜7'
+          },
+          {
+            "image": "/images/bg1.jpg",
+            "title": '蔬菜8'
+          },
+          {
+            "image": "/images/bg1.jpg",
+            "title": '蔬菜9'
+          },
+          {
+            "image": "/images/bg1.jpg",
+            "title": '蔬菜10'
+          }
+        ]
+      },
+      {
+        "banner": "/images/bg1.jpg",
+        "title": '服饰',
+        "product": [
+          {
+            "image": "/images/bg1.jpg",
+            "title": '服饰1'
+          },
+          {
+            "image": "/images/bg1.jpg",
+            "title": '服饰2'
+          },
+          {
+            "image": "/images/bg1.jpg",
+            "title": '服饰3'
+          },
+          {
+            "image": "/images/bg1.jpg",
+            "title": '服饰4'
+          },
+          {
+            "image": "/images/bg1.jpg",
+            "title": '服饰5'
+          },
+          {
+            "image": "/images/bg1.jpg",
+            "title": '服饰6'
+          },
+          {
+            "image": "/images/bg1.jpg",
+            "title": '服饰7'
+          },
+          {
+            "image": "/images/bg1.jpg",
+            "title": '服饰8'
+          },
+          {
+            "image": "/images/bg1.jpg",
+            "title": '服饰9'
+          },
+          {
+            "image": "/images/bg1.jpg",
+            "title": '服饰10'
+          }
+        ]
+      },
+      {
+        "banner": "/images/bg1.jpg",
+        "title": '玩具',
+        "product": [
+          {
+            "image": "/images/bg1.jpg",
+            "title": '水果1'
+          },
+          {
+            "image": "/images/bg1.jpg",
+            "title": '水果2'
+          },
+          {
+            "image": "/images/bg1.jpg",
+            "title": '水果3'
+          },
+          {
+            "image": "/images/bg1.jpg",
+            "title": '水果4'
+          },
+          {
+            "image": "/images/bg1.jpg",
+            "title": '水果5'
+          },
+          {
+            "image": "/images/bg1.jpg",
+            "title": '水果6'
+          },
+          {
+            "image": "/images/bg1.jpg",
+            "title": '水果7'
+          },
+          {
+            "image": "/images/bg1.jpg",
+            "title": '水果8'
+          },
+          {
+            "image": "/images/bg1.jpg",
+            "title": '水果9'
+          },
+          {
+            "image": "/images/bg1.jpg",
+            "title": '水果10'
+          }
+        ]
+      },
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
