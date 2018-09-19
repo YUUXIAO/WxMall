52bfc97cc981d3450a64cb2ceccee3afee58933c
yuxiao <1057305789@qq.com>
2018-09-19 20:09:03 +0800

    修改购物车

--- pages/detail/index.js
+++ pages/detail/index.js
@@ -1,8 +1,5 @@
 // pages/detail/index.js
 Page({
-  /**
-   * 页面的初始数据
-   */
   data: {
     bannerImage: [],
     price: '',
@@ -21,9 +18,9 @@ Page({
     count: 1
   },
   tabClick: function(e) {
-    let current = e.currentTarget.dataset.index;
+    let current = e.currentTarget.dataset.index
     if (this.data.currentTab == current) {
-      return false;
+      return false
     } else {
       this.setData({
         currentTab: current
@@ -37,10 +34,10 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-    let that = this;
+    let that = this
     // 获取商品信息
     wx.request({
-      url: "https://www.easy-mock.com/mock/5b8b9d4a61840c7b40336534/example/goods/detail",
+      url: 'https://www.easy-mock.com/mock/5b8b9d4a61840c7b40336534/example/goods/detail',
       success: function(res) {
         that.setData({
           bannerImage: res.data.bannerImage,
@@ -49,7 +46,8 @@ Page({
           title: res.data.title,
           deliver: res.data.deliver,
           amount: res.data.amount,
-          address: res.data.address
+          address: res.data.address,
+          selected: false
         })
       }
     })
@@ -58,16 +56,16 @@ Page({
     // 创建动画
     this.animation = wx.createAnimation({
       duration: 100,
-      transformOrigin: "50% 50%",
-      timingFunction: "linear",
+      transformOrigin: '50% 50%',
+      timingFunction: 'linear',
       success: function(res) {
         console.log(res)
       }
     })
   },
   showChooseNum: function() {
-    let systemInfo = wx.getSystemInfoSync();
-    this.animation.translate(0, -500 / 750 * systemInfo.windowWidth).step()
+    let systemInfo = wx.getSystemInfoSync()
+    this.animation.translate(0, (-500 / 750) * systemInfo.windowWidth).step()
     this.setData({
       animation: this.animation.export(),
       maskShow: true
@@ -86,7 +84,6 @@ Page({
         duration: 1000
       })
     }
-
   },
   increaseCount: function() {
     var that = this
@@ -95,8 +92,8 @@ Page({
     })
   },
   closeChooseNum: function() {
-    let systemInfo = wx.getSystemInfoSync();
-    this.animation.translate(0, 500 / 750 * systemInfo.windowWidth).step()
+    let systemInfo = wx.getSystemInfoSync()
+    this.animation.translate(0, (500 / 750) * systemInfo.windowWidth).step()
 
     this.setData({
       animation: this.animation.export(),
@@ -106,21 +103,22 @@ Page({
   // 商品加入购物车
   addtoCart: function() {
     let shoppingCart = {
-      "count": this.data.count,
-      "image": this.data.bannerImage[0],
-      "title": this.data.title,
-      "price": this.data.price
+      count: this.data.count,
+      image: this.data.bannerImage[0],
+      title: this.data.title,
+      price: this.data.price,
+      selected: false
     }
-    let self = this;
+    let self = this
     wx.setStorage({
       key: 'shoppingCart',
       data: shoppingCart,
       success: function(res) {
         wx.redirectTo({
-          url: "/pages/index/index"
+          url: '/pages/index/index'
         })
-        let systemInfo = wx.getSystemInfoSync();
-        self.animation.translate(0, 500 / 750 * systemInfo.windowWidth).step()
+        let systemInfo = wx.getSystemInfoSync()
+        self.animation.translate(0, (500 / 750) * systemInfo.windowWidth).step()
         self.setData({
           animation: self.animation.export(),
           maskShow: false
@@ -133,7 +131,5 @@ Page({
         })
       }
     })
-
-
   }
 })
--- pages/shoppingCart/index.js
+++ pages/shoppingCart/index.js
@@ -1,78 +1,154 @@
 // pages/shoppingCart/index.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-    cartList:[]
+    cartList: [],
+    hasList: false,
+    selectAll: false,
+    totalPrice: 0,
+    delBtnWidth: 185
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    var that = this;
+  onLoad: function(options) {
+    var that = this
     wx.getStorage({
       //获取数据的key
       key: 'shoppingCart',
-      success: function (res) {
+      success: function(res) {
         that.setData({
           cartList: that.data.cartList.concat(res.data)
         })
+        // 判断购物车是否有数据
+        console.log(that.data.cartList)
+        console.log(that.data.cartList.length)
+        if (that.data.cartList.length) {
+          that.setData({
+            hasList: true
+          })
+        } else {
+          that.setData({
+            hasList: false
+          })
+        }
       },
-      fail: function (res) {
+      fail: function(res) {
         console.log(res)
       }
     })
   },
+  // 绑定减数量事件
+  reduceCount: function(e) {
+    var that = this
+    var cartList = this.data.cartList
+    var index = e.currentTarget.dataset.index
+    var count = cartList[index].count
+    if (count > 1) {
+      cartList[index].count--
+      that.setData({
+        cartList: cartList
+      })
+    } else {
+      wx.showToast({
+        title: '不能再少了哦！',
+        icon: 'error',
+        duration: 1000
+      })
+    }
+    this.getTotalPrice()
+  },
+  // 绑定加数量事件
+  increaseCount: function(e) {
+    var cartList = this.data.cartList
+    var index = e.currentTarget.dataset.index
+    cartList[index].count++
+    this.setData({
+      cartList: cartList
+    })
+    this.getTotalPrice()
+  },
+  // 选中商品
+  selectGoods: function(e) {
+    let index = e.currentTarget.dataset.index
+    let carts = this.data.cartList
+    let selected = carts[index].selected
+    carts[index].selected = !selected
+    this.setData({
+      cartList: carts
+    })
+    this.getTotalPrice()
+  },
+  // 全选
+  selectAllGoods: function() {
+    var self = this
+    let selectAll = !this.data.selectAll
+    let cartList = this.data.cartList
+    cartList.map(m => {
+      m.selected = selectAll
+    })
+    this.setData({
+      cartList: cartList,
+      selectAll: selectAll
+    })
+    this.getTotalPrice()
+  },
+  // 计算金额
+  getTotalPrice: function() {
+    let cartList = this.data.cartList
+    let total = 0
+    cartList.map(m => {
+      if (m.selected) {
+        total = m.price * m.count
+      }
+    })
+    console.log(total)
+    this.setData({
+      totalPrice: total.toFixed(2)
+    })
+  },
+  // 开始滑动事件
+  touchS: function(e) {},
+  // 滑动中事件
+  touchM: function(e) {},
+  // 滑动中事件
+  touchE: function(e) {},
 
+  // 删除商品
+  delItem: function() {
+    console.log('del')
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-
-  },
+  onReady: function() {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
-  },
+  onShow: function() {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-
-  },
+  onHide: function() {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-
-  },
+  onUnload: function() {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-
-  },
+  onPullDownRefresh: function() {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-
-  },
+  onReachBottom: function() {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-
-  }
+  onShareAppMessage: function() {}
 })
